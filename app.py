8d53c0217be5d47fb7ca9ee04d46ab8ff9b7d9ac
Constanza Osorio <co.osoriou@duocuc.cl>
2024-07-11 20:43:07 -0400

    examen

---
+++ app.py
@@ -0,0 +1,19 @@
+from utilidades import
+
+while true:
+    print("asignar sueldos aleatorios")
+    print("clasificar sueldos")
+    print("ver estadisticas")
+    print("repoprte de sueldos")
+    print("salir del programa ")
+    
+    opcion = input("Seleccione una opcion")
+    
+    if opcion == "1":
+        sueldos = asignar_sueldos_aleatorio
+        print("Sueldos asignados")
+        
+    elif opcion == "2":
+        if sueldos == clasificar_sueldos:
+            print("")
+        
---
+++ utilidades.py
@@ -0,0 +1,29 @@
+import random 
+
+trabajadores= ["Juan Perez","Maria Garcia","Carlos Lopez","Ana Martinez","Pedro Rodriguez","Laura Hernandez","Miguel Sanchez","Isabel Gomez","Francisco Diaz","Elena Fernandez"]
+
+def asignar_sueldos_aleatorio():
+    for trabajador in trabajadores
+        sueldos[trabajador] random.randint(300000, 2500000)
+    print("Sueldos asignados aleatoriamente")  
+          
+def clasificar_sueldos():
+    sueldos_clasificar = sorted(sueldo.items(), key=lambda item: item[1], reverse=true)
+    
+    
+def ver_estadisticas(sueldos):
+    if sueldos:
+        max_sueldo = max(sueldos.values)
+        min_sueldo = min(sueldos.values)
+        promedio_sueldo = sum(sueldos.value)
+        print(f"maximo sueldo: {max_sueldo}")
+        print(f"Minimo sueldo: {minimo_sueldo}")
+        print(f"Promedio de sueldo: {promedio_sueldo}.2f")
+    
+    
+def reporte_sueldos(sueldos):
+    print    
+
+
+def salir:
+    print("Usted salio del programa ")
